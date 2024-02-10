8f132df7f2111675a66900d06241d88267aa880a
Aryan Yadav <yadavaryan2003@gmail.com>
2024-02-10 13:34:56 +0530

    Adding Spinner ~AryanYadav

--- dark_dash/src/index.js
+++ dark_dash/src/index.js
@@ -1,48 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Login from './components/Login/Login';
-import Dashboard from './components/Dashboard';
-import Layout from './components/Layout';
-import ComplaintsTable from './components/ComplaintsTable';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Login from "./components/Login/Login";
+import Dashboard from "./components/Dashboard";
+import Layout from "./components/Layout";
+import ComplaintsTable from "./components/ComplaintsTable";
+import ClipLoader from "react-spinners/ClipLoader";
+import { useState, useEffect } from "react";
 
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/login",
-    element: <Login/>,
+    element: <Login />,
   },
   {
     path: "/",
-    element: <Layout/>,
-    children:[{
-      path:"",
-      element:<Dashboard/>
-    },{
-      path:"complaints",
-      element:<ComplaintsTable/>
-    }]
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Dashboard />,
+      },
+      {
+        path: "complaints",
+        element: <ComplaintsTable />,
+      },
+    ],
   },
-
 ]);
 
+const AppLoader = () => {
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 1500); // Adjust the loading time as needed
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>
-);
+    return () => clearTimeout(loadingTimeout);
+  }, []);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  return isLoading ? (
+    <div className="flex justify-center items-center h-screen text-white">
+      <ClipLoader color="white" size={50} />
+    </div>
+  ) : (
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLoader />);
+
 reportWebVitals();
